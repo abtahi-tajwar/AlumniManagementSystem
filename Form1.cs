888c1361cdd5114b34f147790017245763534564
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-04-16 19:06:19 +0600

    First commit of files

---
+++ Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSFinalProject_University_
+{
+    public partial class LoginForm : Form
+    {
+        bool studentIdPlaceholderText = true;
+        public LoginForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+        }
+
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "Student ID") { 
+                
+            }
+        }
+
+        void passwordReset(object sender, EventArgs e)
+        {
+            if (!password.UseSystemPasswordChar) {
+                password.UseSystemPasswordChar = true;
+                password.Text = "";
+            }
+            
+        }
+        void passwordPlaceholder(object sender, EventArgs e)
+        {
+            if (password.Text == "") {
+                password.Text = "Password";
+                password.UseSystemPasswordChar = false;
+            }
+        }
+        void studentIdReset(object sender, EventArgs e)
+        {
+            if (studentIdPlaceholderText) {
+                txtUsername.Text = "";
+                studentIdPlaceholderText = false;
+            }
+            
+        }
+        void studentIdPlaceholder(object sender, EventArgs e)
+        {
+            if (txtUsername.Text == "")
+            {
+                txtUsername.Text = "Student Id";
+                studentIdPlaceholderText = true;
+            }
+        }
+
+        private void pictureBox8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoginHover(object sender, EventArgs e)
+        {
+            loginButton.Image = CSFinalProject_University_.Properties.Resources.LoginButtonHover2;
+        }
+
+        private void LoginMouseOut(object sender, EventArgs e)
+        {
+            loginButton.Image = CSFinalProject_University_.Properties.Resources.LoginButton;
+        }
+
+        private void formClose(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
