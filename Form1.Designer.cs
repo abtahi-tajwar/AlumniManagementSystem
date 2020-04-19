5b675a132a3c15bd1ae89450df28bce7dd2cb5bb
Abtahi Tajwar <abtahitajwar@gmail.com>
2020-04-19 21:40:51 +0600

    Commited

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -188,7 +188,7 @@
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.Color.Maroon;
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(573, -37);
             this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
